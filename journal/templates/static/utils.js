5f45c4c1d98527e6f9763750a105c5e157512191
Daniel Cano <dcaronm@gmail.com>
2025-08-25 20:03:23 +0200

    Finished the trade details styling

--- journal/templates/static/utils.js
+++ journal/templates/static/utils.js
@@ -1,19 +1,60 @@
 
 
-function formatNumber(num) {
-    if (num >= 1e12) {
-        return `${(num / 1e12).toFixed(2)}T`;
-    } else if (num >= 1e9) {
-        return `${(num / 1e9).toFixed(2)}B`;
-    } else if (num >= 1e6) {
-        return `${(num / 1e6).toFixed(2)}M`;
-    } else if (num >= 1e3) {
-        return `${(num / 1e3).toFixed(2)}K`;
+// Format currency values
+function formatCurrency (value, min_decimals = 1, max_decimals = 2, notation = null) {
+    // Use notation = 'compact' for financial amounts
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: min_decimals,
+        maximumFractionDigits: max_decimals,
+        notation: notation
+    }).format(value);
+}
+
+// Format percentage values
+function formatPercentage (value, min_decimals = 1, max_decimals = 2) {
+    return new Intl.NumberFormat('en-US', {
+        style: 'percent',
+        minimumFractionDigits: min_decimals,
+        maximumFractionDigits: max_decimals
+    }).format(value / 100);
+}
+
+// Format compact values
+function formatFinancial (value, min_decimals = 1, max_decimals = 2, notation = null) {
+    // Use notation = 'compact' for financial amounts
+    return new Intl.NumberFormat('en-US', {
+        minimumFractionDigits: min_decimals,
+        maximumFractionDigits: max_decimals,
+        notation: notation
+    }).format(value);
+}
+
+function formatNumber (n, mode = null, min_decimals = 1, max_decimals = 2, notation = null) {
+    // Use notation = 'compact' for financial amounts
+    if (mode === 'currency') {
+        return formatCurrency(n, min_decimals, max_decimals, notation);
+    } else if (mode === 'percentage') {
+        return formatPercentage(n, min_decimals, max_decimals);
+    } else if (mode == 'financial') {
+        return formatFinancial(n, min_decimals, max_decimals, notation);
+    } else {
+        return n.toFixed(max_decimals);
     }
-    return num.toLocaleString();
 }
 
 function zeroPad (num, length) {
     length = length || 2; // defaults to 2 if no parameter is passed
     return (new Array(length).join('0')+num.toString()).slice(length*-1);
 };
+
+
+function formatDate (dateString) {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('es-ES', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+}
