71f4f6d6c9b8014792836fb87595e9a6c3d575ba
Daniel Cano <Daniel.Cano@bertrandt.com>
2025-09-03 11:27:32 +0200

    Modified some things for fixing the execution in production

--- journal/wsgi.py
+++ journal/wsgi.py
@@ -21,12 +21,10 @@ class PrefixMiddleware:
 
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
-app = DispatcherMiddleware(None, {
-	'/trading-journal': create_app(ProdConfig),
+application = create_app(ProdConfig)
+app = DispatcherMiddleware(application, {
+	'/trading-journal': application,
 })
-
-from werkzeug.middleware.proxy_fix import ProxyFix
-app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 if __name__ == "__main__":
     app.run(port=5002)
